066c65ff01a5f5245130f3a179ca036dff2109ed
Alex <alexokabo2021@gmail.com>
2023-08-16 01:59:11 -0700

    main.c file

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ * @ac: To arg  count
+ * @av: arg of the vector
+ *
+ * Return: 0 on the success, 1 on the error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int kb = 2;
+
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (kb)
+			: "r" (kb));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (kb == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = kb;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
