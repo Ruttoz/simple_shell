7f049b25c721ff279c8c4f259b98be32e22aeaca
Alex <alexokabo2021@gmail.com>
2023-08-16 07:15:52 -0700

    vars.c file

--- vars.c
+++ vars.c
@@ -39,8 +39,8 @@ int is_chain(info_t *info, char *buf, size_t *p)
  * check_chain - checks that we should continue chaining based on last status
  * @info: The parameter  ofstruct
  * @buf: The character buffer
- * @g: Address of current position in buffer
- * @a: starting position in buffer
+ * @p: Address of current position in buffer
+ * @i: starting position in buffer
  * @len: length of buffer
  *
  * Return: void
