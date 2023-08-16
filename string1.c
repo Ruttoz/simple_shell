35139ce0263973db714962a3b95ee9d5518e8914
Alex <alexokabo2021@gmail.com>
2023-08-16 02:17:42 -0700

    string1.c fle

---
+++ string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _strcpy -It  copies string
+ * @dest: The destination
+ * @src: The source
+ *
+ * Return: pointer to the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[a])
+	{
+		dest[a] = src[a];
+		i++;
+	}
+	dest[a] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - It duplicates the string
+ * @str: The string to be duplicated
+ *
+ * Return: pointer to  duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - prints  input string
+ * @str: The string prints
+ *
+ * Return: NULL
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		a++;
+	}
+}
+
+/**
+ * _putchar - Writes the char c to stdout
+ * @c: The char to print
+ *
+ * Return: 1 On success , On error, -1 , and appropriately errno is set .
+ *
+ */
+int _putchar(char c)
+{
+	static int a;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[a++] = c;
+	return (1);
+}
